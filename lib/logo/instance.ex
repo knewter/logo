d34f67e30245d2f407fa08df86c4f3d67f65bd76
Josh Adams <josh@isotope11.com>
2015-05-30 17:52:57 -0500

    Add everything but the drawing...
    OK not everything, but it's everything we need to make the example in
    the README work.

--- lib/logo/instance.ex
+++ lib/logo/instance.ex
@@ -2,13 +2,76 @@ defmodule Logo.Instance do
   use GenServer
   alias __MODULE__
 
+  defmodule Turtle do
+    defstruct [
+      color: nil,
+      pen_down: false,
+      x: 0,
+      y: 0,
+      angle: 0
+    ]
+  end
+
   # Public API
   def start do
-    GenServer.start(Instance, :ok, [])
+    GenServer.start(Instance, %Turtle{}, [])
+  end
+
+  def color(pid, {r, g, b}) do
+    :ok = GenServer.cast(pid, {:color, {r, g, b}})
+    pid
+  end
+
+  def pen_down(pid) do
+    :ok = GenServer.cast(pid, :pen_down)
+    pid
+  end
+
+  def forward(pid, amount) do
+    :ok = GenServer.cast(pid, {:forward, amount})
+    pid
+  end
+
+  def right(pid, angle) do
+    :ok = GenServer.cast(pid, {:angle_delta, angle})
+    pid
+  end
+
+  def left(pid, angle) do
+    :ok = GenServer.cast(pid, {:angle_delta, -1 * angle})
+    pid
+  end
+
+  def get_turtle(pid) do
+    GenServer.call(pid, :get_turtle)
   end
 
   # Server callbacks
-  def init(:ok) do
-    {:ok, []}
+  def init(turtle) do
+    {:ok, turtle}
+  end
+
+  def handle_cast({:color, {r, g, b}}, turtle) do
+    {:noreply, %Turtle{turtle|color: {r, g, b}}}
+  end
+  def handle_cast(:pen_down, turtle) do
+    {:noreply, %Turtle{turtle|pen_down: true}}
+  end
+  def handle_cast({:forward, amount}, turtle) do
+    delta_x = amount * :math.cos(radians(turtle.angle))
+    delta_y = amount * :math.sin(radians(turtle.angle))
+    {:noreply, %Turtle{turtle|x: turtle.x + delta_x, y: turtle.y + delta_y}}
+  end
+  def handle_cast({:angle_delta, angle}, turtle) do
+    angle = rem(360 + turtle.angle + angle, 360)
+    {:noreply, %Turtle{turtle|angle: angle}}
+  end
+
+  def handle_call(:get_turtle, _from, turtle) do
+    {:reply, turtle, turtle}
+  end
+
+  def radians(degrees) do
+    degrees * (:math.pi/180)
   end
 end
--- test/logo/instance_test.exs
+++ test/logo/instance_test.exs
@@ -1,7 +1,62 @@
 defmodule Logo.InstanceTest do
   use ExUnit.Case
+  alias Logo.Instance
 
-  test "can start a logo instance" do
-    assert {:ok, _pid} = Logo.Instance.start
+  setup do
+    {:ok, pid} = Instance.start
+    {:ok, pid: pid}
+  end
+
+  test "can start a logo instance", meta do
+    assert is_pid(meta[:pid])
+  end
+
+  test "can set the color", meta do
+    turtle = meta[:pid]
+            |> Instance.color({255, 0, 0})
+            |> Instance.get_turtle
+    assert {255, 0, 0} = turtle.color
+  end
+
+  test "can put the pen down", meta do
+    turtle = meta[:pid]
+            |> Instance.pen_down
+            |> Instance.get_turtle
+    assert turtle.pen_down
+  end
+
+  test "can move forward", meta do
+    turtle = meta[:pid]
+            |> Instance.forward(400)
+            |> Instance.get_turtle
+    assert_in_delta(400, turtle.x, 0.00001)
+  end
+
+  test "can rotate", meta do
+    turtle = meta[:pid]
+            |> Instance.right(90)
+            |> Instance.get_turtle
+    assert 90 = turtle.angle
+    turtle = meta[:pid]
+             |> Instance.right(360)
+             |> Instance.get_turtle
+    assert 90 = turtle.angle
+    turtle = meta[:pid]
+             |> Instance.left(20)
+             |> Instance.get_turtle
+    assert 70 = turtle.angle
+  end
+
+  test "converting degrees to radians" do
+    assert (3 * :math.pi)/2 == Instance.radians(270)
+  end
+
+  test "walking at an angle", meta do
+    turtle = meta[:pid]
+            |> Instance.right(45)
+            |> Instance.forward(:math.sqrt(2))
+            |> Instance.get_turtle
+    assert_in_delta(1, turtle.x, 0.00001)
+    assert_in_delta(1, turtle.y, 0.00001)
   end
 end
